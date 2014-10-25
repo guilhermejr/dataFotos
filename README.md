cf64399cceec0929b8257c527b1b5475c1cacf04
Guilherme Jr <falecom@guilhermejr.net>
2014-10-25 19:38:36 -0300

    Primeira Versão

---
+++ README.md
@@ -0,0 +1,10 @@
+Estou organizando minhas fotos e criando álbuns no picasa para compartilhar com minha família, só que muitas fotos estão com as datas erradas por que a câmera estava configurado com a data errada na hora que a foto foi tirada.
+
+Para corrigir as datas das fotos criei esse Shell Script que funciona tanto no OS X quando no Linux
+
+Para executar o script basta mudar a permissão do arquivo e executa-lo passando como parâmetro a data, hora e a pasta com as fotos::
+
+	$ chmod 755 dataFotos.sh
+	$ ./dataFotos.sh 25/10/2014 08:00:00 /caminho/das/fotos
+
+Dúvidas e Sugestões favor mandar um e-mail falecom@guilhermejr.net
---
+++ dataFotos.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Desenvolvido por: Guilherme Jr. http://www.guilhermejr.net
+# Licença de uso: GPL
+#
+
+# --- Função para a exibição do erro e sair ---
+erro() {
+   local MSG=$1
+   echo "$MSG" >&2
+   exit 1
+}
+
+# --- Valida número de parâmetros ---
+if [ $# -ne 3 ]; then 
+   erro "Modo de uso: $0 dd/mm/yyyy hh:mm:ss /caminho/das/fotos"
+fi
+
+# --- Valida data e hora ---
+DATA="$1 $2"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+   date -d "$DATA UTC + 1 min" +"%d/%m/%Y %T" &> /dev/null
+   test $? -eq 0 || erro "'$DATA': esta data é inválida."
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+   TEST_DATETIME=$(date -v+1M -v-1M -jf "%d/%m/%Y %T" "$DATA" +"%d/%m/%Y %T" 2> /dev/null)
+   if [[ "$TEST_DATETIME" != "$DATA" ]]; then
+      erro "'$DATA': esta data é inválida."
+   fi
+fi
+
+# --- Valida pasta ---
+PASTA=$3
+if [ ! -d "$PASTA" ]; then
+   erro "Não foi possível acessar a pasta '$PASTA'"
+fi
+
+# --- Modifica a data ---
+exiftool -AllDates=\'"$DATA"\' "$PASTA" 1>/dev/null 2>/dev/null
+
+# --- Apaga os arquivos originais ---
+exiftool -delete_original! "$PASTA" 1>/dev/null 2>/dev/null
